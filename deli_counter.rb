659783af43171dca507a5a4c1d245b9d956a09c2
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-13 23:24:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+katz_deli = []
+
+def place_and_name(katz_deli)
+  counter = 0
+  place = (1..katz_deli.size).to_a
+  while counter < katz_deli.size
+    
+
+def line(customers)
+  if customers.size == 0 
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{}"
+  end
+end
+
+def take_a_number(katz_deli, new_customer)
+end
+
+def now_serving(katz_deli)
+  if katz_deli == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    
+end
