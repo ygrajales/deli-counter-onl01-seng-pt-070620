eb8766a42a4de35e368ead83458423f2a497fd53
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-14 00:38:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,7 @@ def take_a_number(katz_deli, new_customer)
 end
 
 def now_serving(katz_deli)
-  if katz_deli == 0 
+  if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
   else
     nil
